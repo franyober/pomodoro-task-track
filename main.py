3097b7858f93f3e7eb27d36525c83e670d254d42
franyober <franyober123@hotmail.com>
2024-08-26 18:12:32 -0500

    creación automatica de base de datos y tabla en caso de que no se tenga

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from interfaz import *
 
 if __name__ == "__main__":
+
+    if not (controller.os.path.exists(controller.pathDB)):
+        controller.createDB()
+        controller.createTable()
+    else:
+        print("La base de datos ya existe") 
+   
     root = tk.Tk()
     app = PomodoroApp(root)
     root.mainloop()
