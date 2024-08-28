5b39b24c8822648ec42707a0b9c3c273fecb21d6
franyober <franyober123@hotmail.com>
2024-08-28 14:59:44 -0500

    otorgando permisos de ejecución al ejecutable que se crea

--- src/main.py
+++ src/main.py
@@ -10,10 +10,11 @@ if __name__ == "__main__":
 
     if not (controller.os.path.exists(f"{controller.pathProject}/ejecutable")):
         print("No existe el ejecutable")
-        f = open("ejecutable", "w")
+        f = open("ejecutable", "w") # creando archivo ejecutable
         f.writelines(["#!/bin/bash\n", f"cd {controller.pathProject}\n", "source ./env/bin/activate\n",
-        f"python3 {controller.pathProject}/src/main.py\n"])
+        f"python3 {controller.pathProject}/src/main.py\n"]) #agregando las intrucciones a ejecutar
         f.close()
+        print(controller.sh.chmod('u+x', f"{controller.pathProject}/ejecutable")) #otorgando permisos de ejecución
     
     else:
         print("Sí existe el ejecutable")
